24c20e3f4df9e2d43994f712809c3d04df30522f
Nelzon Apaza <84747728+nelzonapa@users.noreply.github.com>
2022-07-31 15:55:17 -0500

    Accediendo a coordenadas mediante indice

--- mouse_control_proyect.py
+++ mouse_control_proyect.py
@@ -53,76 +53,29 @@ with mp_hands.Hands(
     Para el caso en que no se encuentre alguna mano
     """
     if results.multi_hand_landmarks is not None:# en el caso de encontrar una mano
-        #------------------------------------------------------------------
-        #Dibujando los puntos y sus conexiones con mediapipe (leida de todos los puntos)
-        """
-        Recorreremos los 21 puntos por cada mano detectada
-        """
-        #for hand_landmarks in results.multi_hand_landmarks:
-            #print(hand_landmarks)# imprime 21 puntos por cada mano
-        """
-            Dibujando los 21 puntos de los hand_landmarks con ayuda de mediapipe
-            """
-            #mp_drawing.draw_landmarks(image,hand_landmarks,mp_hands.HAND_CONNECTIONS)
-        """
-            Ya se específicó la imagen que se tiene en la variable "imagen" donde se 
-            dibujará los 21 puntos de cada mano. Incluso uno puede usar :
-            multi HANDEDNESS y multi HAND LANDMARKS para adquirir información de puntos o 
-            qué manos se estan detectando en la imagen.
-            OJO los colores fueron establecidos automáticamente
-            """
-        """
-            EN EL CASO DE QUERER CAMBIAR EL COLOR:
-            mp_drawing.draw_landmarks(
-                image,hand_landmarks,mp_hands.HAND_CONNECTIONS,
-                mp_drawing.DrawingSpec(color=(255,255,0),thickness=4,circle_radius=5).
-                mp_drawing.DrawingSpec(color=(255,0,255),thickness=4))
-            """
-        #---------------------------------------------------------------
         #---------------------------------------------------------------
         """
-        Accediendo a los puntos clave(hand landmarks) de acuerdo a su nombre, el cual esta establecido
-        por mediapipe: 
-        Pulgar-> THUMB_TIP 
-        Índice-> INDEX_FINGER_TIP
-        Medio-> MIDDLE_FINGER_TIP
-        Anular-> RING_FINGER_TIP
-        Meñique-> PINKY_TIP
+        Accediendo a los puntos clave(hand landmarks) de acuerdo a indice
+        Pulgar-> THUMB_TIP --> 4
+        Índice-> INDEX_FINGER_TIP --> 8
+        Medio-> MIDDLE_FINGER_TIP --> 12
+        Anular-> RING_FINGER_TIP --> 16
+        Meñique-> PINKY_TIP --> 20
         """
+        #Hacemos una lista index:
+        index = [4,8,12,16,20]
         for hand_landmarks in results.multi_hand_landmarks:
-            x1=int(hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].x*width)
-            """
-            Se Accedió a la coordenada "x" del o de los pulgares
-            Se debe multiplicar el valor de la coordenada x del o los pulgares por el ancho de la imagen
-            para obtener un numero más grande. Finalmente obtenemos su valor sin decimales con int()
-
-            """
-            #Para la coordenada y, lo mismo, solo que multiplicado por el alto de la imagen
-            y1=int(hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].y*height)
-            #print(x1,y1) #mostramos coordenadas del punto 1 en la terminal
-
-            #PROBAMOS las coordenadas obtenidas, dibujando un circulo en las coordenadas
-            cv2.circle(image,(x1,y1),3,(255,0,0),3)
-
-            """
-            Ubicando los demás puntos:
-            """
-            x2=int(hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x*width)
-            y2=int(hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y*height)
-
-            x3=int(hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].x*width)
-            y3=int(hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].y*height)
-
-            x4=int(hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_TIP].x*width)
-            y4=int(hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_TIP].y*height)
-            
-            x5=int(hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_TIP].x*width)
-            y5=int(hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_TIP].y*height)
-
-            cv2.circle(image,(x2,y2),3,(255,0,0),3)
-            cv2.circle(image,(x3,y3),3,(255,0,0),3)
-            cv2.circle(image,(x4,y4),3,(255,0,0),3)
-            cv2.circle(image,(x5,y5),3,(255,0,0),3)
+            # Este for servirá para que recorra cada uno de los 21 puntos de cada mano
+            for (i,points) in enumerate(hand_landmarks.landmark):
+                if i in index:
+                    """
+                    Si i que irá aumentando encuentra a un dato igual a un dato de la lista index
+                    obtenemos las coordenadas:
+                    """
+                    x=int(points.x*width)
+                    y=int(points.y*height)
+                    #dibujamos un circulo en cada coordenada
+                    cv2.circle(image,(x,y),3,(255,0,0),3)
         #---------------------------------------------------------------
     
     #volteamos nuevamente para dejarla con la orientación horiginal
